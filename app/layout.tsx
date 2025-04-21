303c090909a86212fc492bd91d8a995f403e900d
Aqiel Ilhamy <aqiel.ilhamy@commsult.id>
2025-04-21 16:12:38 +0700

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         {/* Include your fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Garamond&display=swap"
-          rel="stylesheet"
+          rel="preload"
         />
       </head>
       <body
